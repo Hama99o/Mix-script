3e2825235d0c2d8dfa9b1a93c0a18f3885d3ca90
hama <hmmshl@gmail.com>
2021-12-11 20:18:30 +0200

    make_checkered_board added

---
+++ make_checkered_board.rb
@@ -0,0 +1,35 @@
+def make_checkered_board(n)
+  result = []
+  x = getArray(n, x, 'X')
+  o = getArray(n, o, 'O')
+  count = 1
+
+  n.times do
+    if count % 2 == 0
+      result.push(o)
+      count += 1
+    elsif
+      result.push(x)
+      count += 1
+    end
+  end
+  p n
+  p result
+  result
+end
+
+
+def getArray (n, x, str)
+  count = str == 'X' ? 1 : 0
+  x = []
+  n.times do
+    if count % 2 == 0
+      x.push('O')
+      count += 1
+    elsif
+      x.push('X')
+      count += 1
+    end
+  end
+  x
+end
