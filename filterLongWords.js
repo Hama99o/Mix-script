9329c503a15f492681a0960fe9feebc1cc01fcff
hama <hmmshl@gmail.com>
2022-05-12 12:42:39 +0200

    filterLongWords added

---
+++ filterLongWords.js
@@ -0,0 +1,11 @@
+function filterLongWords(sentence, n) {
+  const s_array = sentence.split(' ')
+
+
+  return s_array.reduce((result, i ) => {
+    if (i.split('').length > n) {
+      result.push(i)
+    }
+    return result
+  }, [])
+}
