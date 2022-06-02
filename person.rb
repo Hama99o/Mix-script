1ba840743326def019747d3eae47d2408b05f027
hama <hmmshl@gmail.com>
2022-06-02 20:28:38 +0200

    person added

---
+++ person.rb
@@ -0,0 +1,16 @@
+class Person
+  def initialize(firstName, lastName, age)
+    @firstName = firstName
+    @lastName = lastName
+    @age = age
+
+  end
+
+  def full_name
+    "#{@firstName} #{@lastName}"
+  end
+
+  def age
+    @age
+  end
+end
