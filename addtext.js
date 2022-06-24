9298db36250cc17e2fe8b4524392dc87fc854f08
hama <hmmshl@gmail.com>
2022-06-24 15:56:03 +0200

    addtext updated

--- addtext.js
+++ addtext.js
@@ -1,8 +1,8 @@
 let arr = [];
 function addText() {
-    let text = document.getElementById('text').value;
+    let text = document.getElementById('questions_hash_quick_reply').value;
     arr.push(text);
-    res = document.getElementById('result-text').innerText = arr;
+    res = document.getElementById('quick_reply').innerText = arr;
 }
 
 {/* <input type="text" id="text" placeholder="Enter a text" />
