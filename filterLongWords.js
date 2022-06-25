ad2369bd9ea6d4625bfea7747a93805c4b0f50fa
Hama <72570079+Hama99o@users.noreply.github.com>
2022-06-25 13:51:56 +0200

    filterLongWords method  updated

--- filterLongWords.js
+++ filterLongWords.js
@@ -1,7 +1,6 @@
 function filterLongWords(sentence, n) {
   const s_array = sentence.split(' ')
 
-
   return s_array.reduce((result, i ) => {
     if (i.split('').length > n) {
       result.push(i)
