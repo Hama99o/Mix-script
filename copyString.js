1b0a05bd699d58f67213a34cc69b0e33d23c8fc3
hama <hmmshl@gmail.com>
2022-07-11 11:45:42 +0200

    copyString added

---
+++ copyString.js
@@ -0,0 +1,26 @@
+export default {
+  methods: {
+    copyString (text) {
+      const copyText = document.createElement('textarea')
+      copyText.value = text
+      document.body.appendChild(copyText)
+      copyText.select()
+      copyText.setSelectionRange(0, 99999)
+      document.execCommand('copy')
+      document.body.removeChild(copyText)
+    },
+    copyTextConfirm (value) {
+      try {
+        this.copyString(value)
+        this.$fire({
+          title: 'You just copied: ',
+          text: value,
+          type: 'success',
+          timer: 3000
+        })
+      } catch {
+        this.$alert('Failed to copy text')
+      }
+    }
+  }
+}
