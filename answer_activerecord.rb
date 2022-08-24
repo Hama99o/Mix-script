c810dfbb6ab78a1e4a4db61066acd5b332eeac8a
hama <hmmshl@gmail.com>
2022-08-24 10:02:31 +0200

    ar added

--- answer_activerecord.rb
+++ answer_activerecord.rb
@@ -1,14 +1,17 @@
 Answer.select(
   [
-    Answer.arel_table[:message_id].count.as('count'), Arel::Nodes::NamedFunction.new(
+    Answer.arel_table[:message_id].count.as('count'), 
+    Arel::Nodes::NamedFunction.new(
       'SUM', [
         Arel::Nodes::Case.new.when(User.arel_table[:gender].eq('male')).then(1).else(0)
       ]
-    ).as('male'), Arel::Nodes::NamedFunction.new(
+    ).as('male'), 
+    Arel::Nodes::NamedFunction.new(
       'SUM', [
         Arel::Nodes::Case.new.when(User.arel_table[:gender].eq('female')).then(1).else(0)
       ]
-    ).as('female'), Answer.arel_table[:text].as('text')
+    ).as('female'), 
+    Answer.arel_table[:text].as('text')
   ]
 ).where(
   Answer.arel_table[:question_id].eq(Question.arel_table[:id]).and(Answer.arel_table[:kind].eq(nil))
