2b8df81eea65971989ed883c309279a8883ecee8
hama <hmmshl@gmail.com>
2022-09-05 10:02:31 +0200

    buttton added

---
+++ html_vue_buuton.vue
@@ -0,0 +1,6 @@
+<div v-if="isEditable" >
+  <button type="button" :data-checked="checked" class="p-2 px-3 rounded btn btn-success submitButton float-right" style='min-width: max-content' @click="createOrUpdatePush()" :disabled="checked === 'saved' || !canSubmit" >
+    <i :class="saveAndScheduleButtonClass"></i>
+    Save
+  </button>
+</div>
