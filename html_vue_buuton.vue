e547df1a7788ec59bd3dd99b262f1b9beebdbf3b
hama <hmmshl@gmail.com>
2022-09-10 19:09:59 +0200

    button updated

--- html_vue_buuton.vue
+++ html_vue_buuton.vue
@@ -1,4 +1,4 @@
-<div v-if="isEditable" >
+<div v-if="isEditable">
   <button type="button" :data-checked="checked" class="p-2 px-3 rounded btn btn-success submitButton float-right" style='min-width: max-content' @click="createOrUpdatePush()" :disabled="checked === 'saved' || !canSubmit" >
     <i :class="saveAndScheduleButtonClass"></i>
     Save
