1cbadfe417c4463ef63b67fd83db019ca81e745a
hama <hmmshl@gmail.com>
2022-10-06 17:52:59 +0200

    getPollByConversation added

---
+++ getPollByConversation.vue
@@ -0,0 +1,8 @@
+    async getPollByConversation (conversation) {
+      let poll = {}
+
+      if (conversation.poll && conversation.poll.id) {
+        poll = await this.getPoll(conversation.poll.id)
+      }
+      this.goToNewPush(poll, conversation)
+    },
