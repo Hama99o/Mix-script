dd82a84df4c6a5a7c64b2c8ae84e025bc38990a7
hama <hmmshl@gmail.com>
2022-10-09 17:52:59 +0200

    getSgement added

---
+++ getSegmentsData.vue
@@ -0,0 +1,12 @@
+async getSegmentsData () {
+  if (!this.validatedParams) return
+  this.toggleLoading(true)
+  const data = await this.getSegmentData(this.segmentParams)
+  this.toggleLoading(false)
+  this.segment.usersCount = data.usersCount
+  this.segment.availableCities = data.cities
+  this.segment.sql = data.sql
+  this.push.sql = format(this.push.frontParams.segments.map((s) => {
+    return s.sql
+  }).join('\n\nUNION \n\n'))
+}
