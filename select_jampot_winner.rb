e7d0c903611ebf63d0dde5c735df219eb8d0c387
hama <hmmshl@gmail.com>
2022-10-29 09:53:59 +0200

    winner_push added in SelectJampotWinner

---
+++ select_jampot_winner.rb
@@ -0,0 +1,18 @@
+class SelectJampotWinner
+  include Sidekiq::Worker
+
+  private
+
+  def winner_push(user_id)
+    state_machine = StateMachine.find(317)
+
+    push =  push = Push.create!(
+      name: state_machine.name,
+      workflow_state: state_machine.start_state,
+      template: push_template(state_machine.start_state),
+      publish_datetime: Time.now,
+      user_ids: [user_id],
+    )
+    scheduled_push(push, [user_id])
+  end
+end
