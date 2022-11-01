89a5f13d29a593eea76898dce2a310c23ef246de
hama <hmmshl@gmail.com>
2022-11-01 09:53:59 +0200

    losers_push method added

--- select_jampot_winner.rb
+++ select_jampot_winner.rb
@@ -16,6 +16,28 @@ class SelectJampotWinner
     scheduled_push(push, [user_id])
   end
 
+  def losers_push(user_ids)
+    state_machine = StateMachine.find(322)
+
+    push = push = Push.create!(
+      name: state_machine.name,
+      workflow_state: state_machine.start_state,
+      template: push_template(state_machine.start_state),
+      publish_datetime: Time.now,
+      user_ids: user_ids,
+    )
+    scheduled_push(push, user_ids)
+  end
+
+  def notify_winner_user_id_on_slack(user_id)
+    SlackNotificationWorker.perform_async(
+      "The winner user_id is #{user_id}",
+      {
+        channel: '#jampot',
+      }
+    )
+  end
+
   def notify_error_on_slack
     SlackNotificationWorker.perform_async(
       "Jampot failed! qualified users not found",
