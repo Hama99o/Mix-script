9c0c817b181126a87d8bb45f9f7e03f152536c78
hama <hmmshl@gmail.com>
2022-11-07 09:34:19 +0100

    SelectJampotWinner class updated

--- select_jampot_winner.rb
+++ select_jampot_winner.rb
@@ -38,8 +38,6 @@ class SelectJampotWinner
     end
   end
 
-  private
-
   def winner_push(winner_id)
     raise "'#{WINNER_STATE_MACHINE_NAME}' state machine doesn't exist" unless MessengerBot::StateMachine.exists?(WINNER_STATE_MACHINE_NAME)
 
@@ -80,50 +78,59 @@ class SelectJampotWinner
     scheduled_push(push)
   end
 
-  def losers_push(user_ids)
-    state_machine = StateMachine.find(322)
 
-    push = push = Push.create!(
-      name: state_machine.name,
+  def second_loser_push(competition_items)
+    raise "'#{LOSER_STATE_MACHINE_NAME}' state machine doesn't exist" unless MessengerBot::StateMachine.exists?(LOSER_STATE_MACHINE_NAME)
+
+    state_machine ||= MessengerBot::StateMachine.instance.find(LOSER_STATE_MACHINE_NAME)
+    sql_query = second_push_users(competition_items)
+    .select(:user_id)
+    .distinct
+    .to_sql
+
+    publish_datetime = Time.now + 1.hour + 30.minutes
+
+
+    push = Push.create!(
+      name: "#{state_machine.name}_#{publish_datetime.strftime("%H:%M")}",
       workflow_state: state_machine.start_state,
-      template: push_template(state_machine.start_state),
-      publish_datetime: Time.now,
-      user_ids: user_ids,
+      template: { state_machine.start_state => MessengerBot::Wordings.get(state_machine.start_state) },
+      publish_datetime: publish_datetime,
+      sql: sql_query
     )
-    scheduled_push(push, user_ids)
+
+    scheduled_push(push)
   end
 
-
-  def scheduled_push(push, user_ids)
-    MessengerBot::SendPushMessagesWorker.perform_at(push.publish_datetime.to_i, push.id, user_ids, nil, nil)
+  def scheduled_push(push)
+    MessengerBot::SendPushMessagesWorker.perform_at(push.publish_datetime.to_i, push.id, nil, nil, push.sql)
     push.update(scheduled: true)
   end
 
-  def winner_user_id_draw(user_ids_with_scores)
-    max_score = user_ids_with_scores.values.sum
+
+  def winner_publish_datetime(id)
+    one_hour_before = (Time.now).strftime("%H:%M").to_i * 3600
+    one_hour_after = (Time.now + 1.hour).strftime("%H:%M").to_i * 3600
+    user = User.find(id)
+    user.morning_push_hour >= one_hour_before && user.morning_push_hour < one_hour_after ? Time.now + 30.minutes + 1.hour : Time.now + 30.minutes
+  end
+
+  def draw_winner_id(score_group_by_user_ids)
+    max_score = score_group_by_user_ids.values.sum
 
     if max_score >= 1
       random_score = rand(1..max_score)
 
-      user_ids_with_scores.each do |user_id, user_score|
+      score_group_by_user_ids.each do |user_id, user_score|
         return user_id if random_score <= user_score
         random_score -= user_score
       end
     end
   end
 
-  def notify_winner_user_id_on_slack(user_id)
+  def notify_slack(text)
     SlackNotificationWorker.perform_async(
-      "The winner user_id is #{user_id}",
-      {
-        channel: '#jampot',
-      }
-    )
-  end
-
-  def notify_error_on_slack
-    SlackNotificationWorker.perform_async(
-      "Jampot failed! qualified users not found",
+     text,
       {
         channel: '#jampot',
       }
@@ -131,9 +138,18 @@ class SelectJampotWinner
   end
 
   def push_template(state)
-    MessengerBot::Wordings.instance.load! # for relaoding the wording
-    template = {}
-    template[state] = MessengerBot::Wordings.get(state)
-    template
+    { state => MessengerBot::Wordings.get(state) }
+  end
+
+  def first_push_users(competition_items)
+    one_hour_before = (Time.now).strftime("%H:%M").to_i * 3600
+    one_hour_after = (Time.now + 1.hour).strftime("%H:%M").to_i * 3600
+    competition_items.joins(:user).where("users.morning_push_hour <  #{one_hour_before} OR users.morning_push_hour > #{one_hour_after}")
+  end
+
+  def second_push_users(competition_items)
+    one_hour_before = (Time.now).strftime("%H:%M").to_i * 3600
+    one_hour_after = (Time.now + 1.hour).strftime("%H:%M").to_i * 3600
+    competition_items.joins(:user).where("users.morning_push_hour >= #{one_hour_before} AND users.morning_push_hour < #{one_hour_after}")
   end
 end
