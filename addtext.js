7f5ae1edb8318e231531dd2a604cdabde4a59fda
Hama <72570079+Hama99o@users.noreply.github.com>
2022-11-26 09:24:23 +0100

    comments removed

--- addtext.js
+++ addtext.js
@@ -4,8 +4,3 @@ function addText() {
     arr.push(text);
     res = document.getElementById('quick_reply').innerText = arr;
 }
-
-{/* <input type="text" id="text" placeholder="Enter a text" />
-<input type="submit" onclick="addText()" />
-
-<p id="result-text"></p>   */}
