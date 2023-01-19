4017463676c1ec7e7bb9e5a4cb18787817a28337
hama <hmmshl@gmail.com>
2023-01-19 20:40:57 +0100

    app added

---
+++ app.js
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="d-flex flex-column">
+    <background-animation />
+    <router-view> </router-view>
+  </div>
+</template>
+
+<script>
+import BackgroundAnimation from './components/layouts/BackgroundAnimation.vue'
+export default {
+  name: 'App',
+  components: { BackgroundAnimation }
+}
+</script>
+
+<style>
+.nightmode {
+  background: #292b2c;
+  color: #eee;
+}
+* {
+  position: relative;
+}
+</style>
