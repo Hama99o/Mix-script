97a493e4db55ba844295a65affb5e6cdaee50c22
hama <hmmshl@gmail.com>
2023-02-05 16:46:38 +0100

    validEnv added

---
+++ insights_data/validEnv.js
@@ -0,0 +1,9 @@
+  if (!validEnv.includes(currentEnv)) {
+    throw new Error(
+      'Please specify a valid `NODE_ENV` or ' +
+        '`BABEL_ENV` environment variables. Valid values are "development", ' +
+        '"test", and "production". Instead, received: ' +
+        JSON.stringify(currentEnv) +
+        '.'
+    )
+  }
